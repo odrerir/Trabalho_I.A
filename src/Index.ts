4ab48f5e9e54068fcdd3b2f4d8cbe23f2eda1911
odrerir <victorcaua03@gmail.com>
2024-05-17 16:27:18 -0300

    Update Index.ts

--- src/Index.ts
+++ src/Index.ts
@@ -15,17 +15,22 @@ const vertices: { [key: string]: Vertex } = {
 };
 
 // Adicionar vizinhos (arestas)
-vertices.A.addNeighbor(vertices.C, 2); // Custo de A para C é 2
-vertices.A.addNeighbor(vertices.E, 1); // Custo de A para E é 1
-vertices.B.addNeighbor(vertices.D, 3); // Custo de B para D é 3
-vertices.C.addNeighbor(vertices.F, 2); // Custo de C para F é 2
-vertices.D.addNeighbor(vertices.K, 4); // Custo de D para K é 4
-vertices.E.addNeighbor(vertices.K, 2); // Custo de E para K é 2
-vertices.F.addNeighbor(vertices.H, 3); // Custo de F para H é 3
-vertices.G.addNeighbor(vertices.A, 10); // Custo de G para A é 10
-vertices.G.addNeighbor(vertices.B, 7);  // Custo de G para B é 7
-vertices.G.addNeighbor(vertices.C, 8);  // Custo de G para C é 8
-vertices.H.addNeighbor(vertices.K, 2);  // Custo de H para K é 2
+vertices.A.addNeighbor(vertices.B, 5); 
+vertices.A.addNeighbor(vertices.H, 10); 
+vertices.A.addNeighbor(vertices.D, 10); 
+vertices.B.addNeighbor(vertices.F, 5); 
+vertices.C.addNeighbor(vertices.D, 5); 
+vertices.D.addNeighbor(vertices.E, 5); 
+vertices.D.addNeighbor(vertices.G, 10); 
+vertices.E.addNeighbor(vertices.C, 10); 
+vertices.E.addNeighbor(vertices.A, 5);  
+vertices.E.addNeighbor(vertices.K, 10);  
+vertices.F.addNeighbor(vertices.G, 15); 
+vertices.G.addNeighbor(vertices.A, 10); 
+vertices.G.addNeighbor(vertices.C, 5);
+vertices.H.addNeighbor(vertices.B, 5);
+vertices.H.addNeighbor(vertices.K, 20);
+vertices.K.addNeighbor(vertices.B, 10);
 
 // Definir vértices de início e fim
 const start = vertices.G;
